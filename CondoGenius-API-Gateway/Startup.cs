f614e6ec89efdf6345e46a908032fb0bb41df256
Igor Mucharski <igor.m99@hotmail.com>
2023-08-23 16:00:49 -0300

    fix: fixed API Gateway and deleted node_modules

---
+++ CondoGenius-API-Gateway/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace CondoGenius_API_Gateway;
+
+public class Startup
+{
+    private readonly IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        this._configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddOcelot(_configuration);
+    }
+
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseOcelot().Wait();
+    }
+}
---
+++ CondoGenius-Checks-API/Controllers/StatusController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CondoGenius_Checks_API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class StatusController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Status()
+    {
+        return Ok("Checks API is online!");
+    }
+}
