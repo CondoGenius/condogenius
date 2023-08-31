df4fa8fb06ad5c975166a39aeea7137732372584
hellen gurgacz <hellen.gurgacz@g.globo>
2023-08-31 15:08:49 -0300

    add

--- CondoGenius-WEB/src/components/residents/containers/actions/resident_actions.jsx
+++ CondoGenius-WEB/src/components/residents/containers/actions/resident_actions.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { NavLink } from 'react-router-dom';
+import React from "react";
 
 import './resident_actions.scss';
 import { Button } from "react-materialize";
