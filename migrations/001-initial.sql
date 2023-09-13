5c40e390b2ebbd4818a95b2e943d1b30aa6eda83
ViniciusLisboa07 <viniciuslisboa1001@gmail.com>
2023-09-13 08:44:01 -0300

    ajustes

--- migrations/001-initial.sql
+++ migrations/001-initial.sql
@@ -4,8 +4,13 @@
 
 -- use genius;
 
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS  users (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     is_active BOOLEAN NOT NULL,
@@ -13,46 +18,43 @@ CREATE TABLE users (
     FOREIGN KEY (role_id) REFERENCES roles (id)
 );
 
-CREATE TABLE roles (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) UNIQUE NOT NULL
-);
-
-CREATE TABLE residents (
-    user_id (int) NOT NULL FOREIGN KEY,
-	residence_id (int) NOT NULL, 
-	cpf_cnpj varchar(20) NOT NULL, 
-	email varchar(255) NOT NULL, 
-	name varchar(255) NOT NULL, 
-	last_name varchar(255) NOT NULL, 
-	contact varchar(20) NOT NULL, 
-	created_at datetime,
-	updated_at datetime, 
-	is_active boolean NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (id),
-)
-
-CREATE TABLE residences (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS residences (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     number INTEGER NOT NULL,
     floor INTEGER NOT NULL,
     block INTEGER NOT NULL,
     complement VARCHAR(255) NOT NULL,
     created_at DATETIME NOT NULL,
-    updated_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL
 );
 
-CREATE TABLE delivery_control (
-    id SERIAL PRIMARY KEY,
-    resident_id INTEGER NOT NULL FOREIGN KEY,
+CREATE TABLE IF NOT EXISTS residents (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    user_id  BIGINT UNSIGNED NOT NULL,
+	residence_id INTEGER NOT NULL, 
+	cpf_cnpj VARCHAR(255) NOT NULL, 
+	email VARCHAR(255) NOT NULL, 
+	name VARCHAR(255) NOT NULL, 
+	last_name VARCHAR(255) NOT NULL, 
+	contact VARCHAR(255) NOT NULL, 
+	created_at DATETIME,
+	updated_at DATETIME, 
+	is_active BOOLEAN NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (residence_id) REFERENCES residences (id)
+);
+
+CREATE TABLE IF NOT EXISTS delivery_control (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    resident_id INTEGER NOT NULL,
     created_at DATETIME NOT NULL,
     updated_at DATETIME NOT NULL,
     received BOOLEAN NOT NULL,
     FOREIGN KEY (resident_id) REFERENCES residents (id)
 );
 
-CREATE TABLE common_area (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS common_area (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     capacity INTEGER NOT NULL,
     business_hour VARCHAR(255) NOT NULL,
@@ -61,10 +63,10 @@ CREATE TABLE common_area (
     updated_at DATETIME NOT NULL
 );
 
-CREATE TABLE reserve_common_area (
-    id SERIAL PRIMARY KEY,
-    common_area_id INTEGER NOT NULL FOREIGN KEY,
-    user_id INTEGER NOT NULL FOREIGN KEY,
+CREATE TABLE IF NOT EXISTS reserve_common_area (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    common_area_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
     reserve_date DATETIME NOT NULL,
     created_at DATETIME NOT NULL,
     updated_at DATETIME NOT NULL,
@@ -72,19 +74,19 @@ CREATE TABLE reserve_common_area (
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
-CREATE TABLE check_in_common_area (
-    id SERIAL PRIMARY KEY,
-    common_area_id INTEGER NOT NULL FOREIGN KEY,
-    resident_id INTEGER NOT NULL FOREIGN KEY,
+CREATE TABLE IF NOT EXISTS check_in_common_area (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    common_area_id INTEGER NOT NULL,
+    resident_id INTEGER NOT NULL,
     status boolean NOT NULL,
     created_at DATETIME NOT NULL,
     FOREIGN KEY (common_area_id) REFERENCES common_area (id),
     FOREIGN KEY (resident_id) REFERENCES residents (id)
 );
 
-CREATE TABLE posts (
-    id SERIAL PRIMARY KEY,
-    resident_id INTEGER NOT NULL FOREIGN KEY,
+CREATE TABLE IF NOT EXISTS posts (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    resident_id INTEGER NOT NULL,
     title VARCHAR(255) NOT NULL,
     content VARCHAR(255) NOT NULL,
     fixed BOOLEAN NOT NULL,
@@ -94,10 +96,10 @@ CREATE TABLE posts (
 );
 
 
-CREATE TABLE comments (
-    id SERIAL PRIMARY KEY,
-    post_id INTEGER NOT NULL FOREIGN KEY,
-    resident_id INTEGER NOT NULL FOREIGN KEY,
+CREATE TABLE IF NOT EXISTS comments (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    post_id INTEGER NOT NULL,
+    resident_id INTEGER NOT NULL,
     content VARCHAR(255) NOT NULL,
     created_at DATETIME NOT NULL,
     updated_at DATETIME NOT NULL,
@@ -106,9 +108,9 @@ CREATE TABLE comments (
 );
 
 
-CREATE TABLE poll (
-    id SERIAL PRIMARY KEY,
-    post_id INTEGER NOT NULL FOREIGN KEY,
+CREATE TABLE IF NOT EXISTS poll (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    post_id INTEGER NOT NULL,
     title VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
     created_at DATETIME NOT NULL,
@@ -116,9 +118,9 @@ CREATE TABLE poll (
     FOREIGN KEY (post_id) REFERENCES posts (id)
 );
 
-CREATE TABLE poll_options (
-    id SERIAL PRIMARY KEY,
-    poll_id INTEGER NOT NULL FOREIGN KEY,
+CREATE TABLE IF NOT EXISTS poll_options (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    poll_id INTEGER NOT NULL,
     title VARCHAR(255) NOT NULL,
     created_at DATETIME NOT NULL,
     updated_at DATETIME NOT NULL,
@@ -126,30 +128,30 @@ CREATE TABLE poll_options (
 );
 
 -- OPTEI POR CRIAR UMA TABELA DE VOTOS PARA QUE POSSA SER FEITO UM HISTÓRICO DE VOTOS, SEM QUE SEJA NECESSARIO DESSERIALIZAÇÃO PRA FAZER CONSULTAS
-CREATE TABLE poll_votes (
-    id SERIAL PRIMARY KEY,
-    poll_option_id INTEGER NOT NULL FOREIGN KEY,
-    resident_id INTEGER NOT NULL FOREIGN KEY,
+CREATE TABLE IF NOT EXISTS poll_votes (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    poll_option_id INTEGER NOT NULL,
+    resident_id INTEGER NOT NULL,
     created_at DATETIME NOT NULL,
     updated_at DATETIME NOT NULL,
     FOREIGN KEY (poll_option_id) REFERENCES poll_options (id),
     FOREIGN KEY (resident_id) REFERENCES residents (id)
 );
 
-CREATE TABLE complaints (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS complaints (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    user_id INTEGER NOT NULL FOREIGN KEY,
+    user_id INTEGER NOT NULL,
     description VARCHAR(255) NOT NULL,
-    user_to_complaint_id INTEGER NOT NULL FOREIGN KEY,
+    user_to_complaint_id INTEGER NOT NULL,
     status varchar(255) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (user_to_complaint_id) REFERENCES users (id)
 );
 
-CREATE TABLE notifications (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL FOREIGN KEY,
+CREATE TABLE IF NOT EXISTS notifications (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    user_id INTEGER NOT NULL,
     title VARCHAR(255) NOT NULL,
     type VARCHAR(255) NOT NULL,
     type_id INTEGER NOT NULL,
@@ -158,11 +160,11 @@ CREATE TABLE notifications (
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
-CREATE TABLE fast_list (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS fast_list (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     phone VARCHAR(255) NOT NULL,
-    user_id INTEGER NOT NULL FOREIGN KEY,
+    user_id INTEGER NOT NULL,
     status BOOLEAN NOT NULL,
     created_at DATETIME NOT NULL,
     updated_at DATETIME NOT NULL,
@@ -170,8 +172,8 @@ CREATE TABLE fast_list (
 );
 
 
-CREATE TABLE events (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS events (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
     image VARCHAR(255) NOT NULL,
@@ -181,10 +183,10 @@ CREATE TABLE events (
     updated_at DATETIME NOT NULL
 );
 
-CREATE TABLE guest_list (
-    id SERIAL PRIMARY KEY,
-    event_id INTEGER NOT NULL FOREIGN KEY,
-    user_id INTEGER NOT NULL FOREIGN KEY,
+CREATE TABLE IF NOT EXISTS guest_list (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    event_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
     name VARCHAR(255) NOT NULL,
     phone VARCHAR(255) NOT NULL,
     cpf VARCHAR(255) NOT NULL,
@@ -193,4 +195,3 @@ CREATE TABLE guest_list (
     FOREIGN KEY (event_id) REFERENCES events (id),
     FOREIGN KEY (user_id) REFERENCES users (id)
 );
-)
