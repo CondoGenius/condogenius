b5743949cc6a98b9222872107f92cf574417e93d
ViniciusLisboa07 <viniciuslisboa1001@gmail.com>
2023-10-08 14:22:58 -0300

    gateway residents

--- CondoGenius-Gateway-API/index.js
+++ CondoGenius-Gateway-API/index.js
@@ -7,7 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/residents", proxy("http://localhost:8001"));
+
+
+app.use("/api/residents", proxy("http://localhost:7008"));
 
 app.listen(5000, () => {
   console.log("Gateway is Listening to Port 5000");
