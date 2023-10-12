db9227f17af4d113bef92a45221ad22e7d51bd88
ViniciusLisboa07 <viniciuslisboa1001@gmail.com>
2023-10-11 23:07:52 -0300

    ajustes gateway

--- CondoGenius-Gateway-API/index.js
+++ CondoGenius-Gateway-API/index.js
@@ -4,12 +4,16 @@ const proxy = require("express-http-proxy");
 
 const app = express();
 
+const authMiddleware = require("./middlewares/auth");
+
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  authMiddleware(req, res, next)
+});
 
-
-app.use("/api/residents", proxy("http://localhost:7008"));
+app.use("/", proxy("residents:7008"));
 
 app.listen(5000, () => {
   console.log("Gateway is Listening to Port 5000");
